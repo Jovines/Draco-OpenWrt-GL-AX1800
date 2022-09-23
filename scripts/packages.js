f39a91dd8648a56528068396cff98ccd09304b3d
Jovines <1246634075@qq.com>
2022-09-23 08:30:55 +0800

    自定义编译

--- scripts/packages.js
+++ scripts/packages.js
@@ -4,7 +4,7 @@
  * @name app 名称
  * @desc app 描述
  */
-module.exports = [
+ module.exports = [
   {
     name: 'luci-theme-argon',
     desc: 'argon主题',
@@ -33,10 +33,10 @@ module.exports = [
     name: 'luci-app-smartdns',
     desc: 'smartdns DNS防污染',
   },
-  {
-    name: 'luci-app-openclash',
-    desc: 'clash的图形代理软件',
-  },
+  // {
+  //   name: 'luci-app-openclash',
+  //   desc: 'clash的图形代理软件',
+  // },
   {
     name: 'luci-app-aria2',
     desc: 'aria2下载工具',
@@ -49,18 +49,18 @@ module.exports = [
     name: 'luci-app-turboacc',
     desc: 'Turbo ACC 网络加速(支持 Fast Path 或者 硬件 NAT)',
   },
-  {
-    name: 'luci-app-xlnetacc',
-    desc: '迅雷快鸟',
-  },
+  // {
+  //   name: 'luci-app-xlnetacc',
+  //   desc: '迅雷快鸟',
+  // },
   {
     name: 'luci-app-appfilter',
     desc: '应用过滤'
   },
-  {
-    name: 'luci-app-cpufreq',
-    desc: 'CPU 性能优化调节',
-  },
+  // {
+  //   name: 'luci-app-cpufreq',
+  //   desc: 'CPU 性能优化调节',
+  // },
   // {
   //   name: 'luci-app-samba4',
   //   desc: 'samba4文件共享',
@@ -89,18 +89,18 @@ module.exports = [
   //   name: 'luci-app-passwall',
   //   desc: 'passwall代理软件',
   // },
-  // {
-  //   name: 'luci-app-ssr-plus',
-  //   desc: 'ssr-plus 代理软件',
-  // },
-  // {
-  //   name: 'luci-app-aliddns',
-  //   desc: '阿里云ddns插件',
-  // },
-  // {
-  //   name: 'luci-app-ddns',
-  //   desc: '动态域名 DDNS',
-  // },
+  {
+    name: 'luci-app-ssr-plus',
+    desc: 'ssr-plus 代理软件',
+  },
+  {
+    name: 'luci-app-aliddns',
+    desc: '阿里云ddns插件',
+  },
+  {
+    name: 'luci-app-ddns',
+    desc: '动态域名 DDNS',
+  },
   // {
   //   name: 'luci-app-eqos',
   //   desc: '依IP地址限速',
@@ -121,26 +121,26 @@ module.exports = [
   //   name: 'luci-app-mwan3helper',
   //   desc: ' MWAN3分流助手',
   // },
-  // {
-  //   name: 'luci-app-wrtbwmon',
-  //   desc: '实时流量监测',
-  // },
   {
-    name: 'luci-app-serverchan',
-    desc: '微信/Telegram推送',
-  },
-  {
-    name: 'luci-app-transmission',
-    desc: 'BT下载工具',
-  },
-  {
-    name: 'luci-app-jd-dailybonus',
-    desc: '京东签到服务',
+    name: 'luci-app-wrtbwmon',
+    desc: '实时流量监测',
   },
   // {
-  //   name: 'luci-app-frpc',
-  //   desc: 'frpc 网络代理',
+  //   name: 'luci-app-serverchan',
+  //   desc: '微信/Telegram推送',
   // },
+  // {
+  //   name: 'luci-app-transmission',
+  //   desc: 'BT下载工具',
+  // },
+  // {
+  //   name: 'luci-app-jd-dailybonus',
+  //   desc: '京东签到服务',
+  // },
+  {
+    name: 'luci-app-frpc',
+    desc: 'frpc 网络代理',
+  },
   // {
   //   name: 'luci-app-frps',
   //   desc: 'frps 网络代理',
